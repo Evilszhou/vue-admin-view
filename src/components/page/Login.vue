b82c3c8a4fc5ca7101551f17bd5630c0d9b5f8d9
CatoyNan <xjn17858906105@gmail.com>
2019-06-04 17:07:52 +0800

    修改项目名字为电子档案管理系统

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -1,7 +1,7 @@
  <template>
     <div class="login-wrap">
         <div class="ms-login">
-            <div class="ms-title">后台管理系统</div>
+            <div class="ms-title">电子档案管理系统</div>
             <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-content">
                 <el-form-item prop="username">
                     <el-input v-model="ruleForm.username" placeholder="username">
