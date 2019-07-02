d2c4850e2eb0d86d896cd3bf883cfcd58c9b93c6
gaofushuai12138 <3499606002@qq,com>
2019-07-02 10:48:53 +0800

    新增文件

--- src/components/page/Upload.vue
+++ src/components/page/Upload.vue
@@ -22,47 +22,30 @@
         <div class="el-upload__tip" slot="tip">文件不能超过100M</div>
       </el-upload>
        <el-form :model="form" style="width:30%;position:fixed;top:260px;left:600px;height:200px">
-    <!-- <el-form-item label="部门" :label-width="formLabelWidth">
-     <el-select v-model="form.department" placeholder="请选择处事">
-        <el-option label="行政部" value="行政部"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item label="处事" :label-width="formLabelWidth">
-      <el-select v-model="form.region" placeholder="请选择处事">
-        <el-option label="行政部" value="行政部"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
-    </el-form-item> -->
     <el-form-item label="类别:" :label-width="formLabelWidth">
-       <!-- <el-select
-    v-model="value"
-    multiple
-    filterable
-    allow-create
-    focus="popWindows"
-    default-first-option
-    placeholder="请选择文章标签">
-    <el-option
-      v-for="item in options"
-      :key="item.value"
-      :label="item.label"
-      :value="item.value">
-    </el-option>
-  </el-select> -->
+
      <el-input readonly="true" style="width:300px" placeholder="请选择文件类型" v-model="value">
-        
         
       </el-input>
       <el-button type="primary" style="margin-left:320px;margin-top:-32px;display: flex" @click="chooseType">选择类别</el-button>
     </el-form-item>
-    <el-form-item label="文件编号:" :label-width="formLabelWidth">
-      <el-input v-model="form.number" placeholder="请输入内容"></el-input>
+    <el-form-item label="文件来源" :label-width="formLabelWidth" >
+      <el-select v-model="form.fileSource"  @change="getSouce(form.fileSource)">
+        <el-option
+        style="width:300px"
+        v-for="item in fileSources"
+        :key="item.id"
+        :value="item.source_name"
+        ></el-option>
+      </el-select>
     </el-form-item>
+    <!-- <el-form-item label="文件编号:" :label-width="formLabelWidth">
+      <el-input v-model="form.number" placeholder="请输入内容"></el-input>
+    </el-form-item> -->
          <el-form-item label="文件页数:" :label-width="formLabelWidth">
            <el-input v-model="form.pageNum" placeholder="请输页数"></el-input>
          </el-form-item>
-    <el-form-item label="日期:" :label-width="formLabelWidth">
+    <el-form-item label="形成日期:" :label-width="formLabelWidth">
       <el-date-picker
       v-model="form.date"
       type="date"
@@ -78,7 +61,6 @@
           <el-tag
             style="cursor:pointer"
             :key="tag"
-            
             v-for="tag in form.tags"
             @click="chooseTag(tag)"
             :type="tag.type"
@@ -131,8 +113,6 @@
           <el-button type="primary" @click="uploadAnneix">确 定</el-button>
         </span>
       </el-dialog>
-
-
       <el-dialog
   title="选择文本分类"
   :visible.sync="dialogVisible3"
@@ -152,10 +132,6 @@
     <el-button type="primary" @click="confimType">确 定</el-button>
   </span>
 </el-dialog>
-
-
-
-
   </div>
 </template>
 
@@ -164,7 +140,7 @@ import { postJsonRequest, postRequest, getRequest } from "../../main.js";
 import VueCropper from "vue-cropperjs";
 import { fail } from "assert";
 import { setTimeout } from "timers";
-import moment from 'moment';
+import moment from "moment";
 export default {
   name: "upload",
   inject: ["reload"],
@@ -172,53 +148,54 @@ export default {
     return {
       data: [
         {
-            "superId": 0,
-            "children": [
+          superId: 0,
+          children: [
+            {
+              superId: 19,
+              children: null,
+              list: null,
+              id: 22,
+              label: "表情包"
+            },
+            {
+              superId: 19,
+              children: [
                 {
-                    "superId": 19,
-                    "children": null,
-                    "list": null,
-                    "id": 22,
-                    "label": "表情包"
-                },
-                {
-                    "superId": 19,
-                    "children": [
-                        {
-                            "superId": 26,
-                            "children": null,
-                            "list": null,
-                            "id": 27,
-                            "label": "ppt素材"
-                        }
-                    ],
-                    "list": null,
-                    "id": 26,
-                    "label": "透明素材"
+                  superId: 26,
+                  children: null,
+                  list: null,
+                  id: 27,
+                  label: "ppt素材"
                 }
-            ],
-            "list": null,
-            "id": 19,
-            "label": "图片"
+              ],
+              list: null,
+              id: 26,
+              label: "透明素材"
+            }
+          ],
+          list: null,
+          id: 19,
+          label: "图片"
         },
         {
-            "superId": 0,
-            "children": null,
-            "list": null,
-            "id": 20,
-            "label": "背景图"
+          superId: 0,
+          children: null,
+          list: null,
+          id: 20,
+          label: "背景图"
         },
         {
-            "superId": 0,
-            "children": null,
-            "list": null,
-            "id": 21,
-            "label": "风景图"
+          superId: 0,
+          children: null,
+          list: null,
+          id: 21,
+          label: "风景图"
         }
-    ],
-      dialogVisible3:false,
-      dialogVisible4:false,
+      ],
+      dialogVisible3: false,
+      dialogVisible4: false,
       value: [],
+      value1: "",
       table: {
         department: "",
         region: "",
@@ -226,8 +203,19 @@ export default {
         date: "",
         number: "",
         tags: [],
-        pageNum: ""
+        pageNum: "",
+        fileSource:""
       },
+      fileSources: [
+        {
+          fileSourceId: 1,
+          source_name: "中央"
+        },
+        {
+          fileSourceId: 2,
+          source_name: "省级"
+        }
+      ],
       danger: "normal",
       dialogVisible: false,
       config: {
@@ -245,6 +233,7 @@ export default {
         type: "",
         date: "",
         number: "",
+        fileSource:"",
         tags: [],
         pageNum: ""
       },
@@ -269,20 +258,20 @@ export default {
     this.getDocLabelsTree();
   },
   computed: {
-    uploadFile:function(){
-      let url = ""
-      if(process.env.NODE_ENV === 'development'){
+    uploadFile: function() {
+      let url = "";
+      if (process.env.NODE_ENV === "development") {
         url = "/api/uploadFile";
-      }else{
+      } else {
         url = "/uploadFile";
       }
-      return url;       
+      return url;
     },
-    uploadsannex:function(){
+    uploadsannex: function() {
       let url = "";
-      if(process.env.NODE_ENV === 'development'){
+      if (process.env.NODE_ENV === "development") {
         url = "/api/uploadannex";
-      }else{
+      } else {
         url = "/uploadannex";
       }
       return url;
@@ -291,15 +280,18 @@ export default {
   components: {
     VueCropper
   },
- 
+
   methods: {
+    getSouce(item) {
+      console.log(item);
+    },
     getDocLabelsTree() {
       let url = "";
-      if(process.env.NODE_ENV === 'development'){
+      if (process.env.NODE_ENV === "development") {
         url = "/api/public/getDocLabelsTree";
-      }else{
+      } else {
         url = "/public/getDocLabelsTree";
-      }       
+      }
       getRequest(url)
         .then(result => {
           if (result.data.code === 200) {
@@ -314,20 +306,19 @@ export default {
           console.log(e);
         });
     },
-    confimType(){
+    confimType() {
       this.value = [];
       let aKey = this.$refs.tree.getCheckedNodes();
-      console.log(aKey)
-      for(let i = 0;i < aKey.length;i++){
+      console.log(aKey);
+      for (let i = 0; i < aKey.length; i++) {
         this.value.push(aKey[i].label);
       }
       this.dialogVisible3 = false;
     },
-    chooseType(){
+    chooseType() {
       this.dialogVisible3 = true;
-
     },
-    popWindows(){
+    popWindows() {
       this.dialogVisible = true;
     },
     chooseTag(item) {
@@ -338,11 +329,10 @@ export default {
         item.type = "normal";
       }
     },
-    wrong(err){
+    wrong(err) {
       console.log(err);
-
     },
-     open6(msg) {
+    open6(msg) {
       if (msg == undefined) {
         msg = "这是一条错误的提示消息";
       }
@@ -354,11 +344,11 @@ export default {
     getAllTags() {
       let _this = this;
       let url = "";
-      if(process.env.NODE_ENV === 'development'){
+      if (process.env.NODE_ENV === "development") {
         url = "/api/public/getAllTags";
-      }else{
+      } else {
         url = "/public/getAllTags";
-      }       
+      }
       postJsonRequest(url)
         .then(result => {
           console.log(result);
@@ -376,8 +366,8 @@ export default {
         })
         .catch(err => {});
     },
-    beforeUpload() {
-      console.log(this.fileList);
+    beforeUpload(file) {
+      console.log(file);
     },
     open3(msg) {
       this.$notify({
@@ -387,8 +377,8 @@ export default {
       });
     },
     success(res) {
-      console.log(res)
-      this.open6(res.msg)
+      console.log(res);
+      this.open6(res.msg);
       console.log(this.form.tags);
       if (res.code == 200) {
         this.open3(res.msg);
@@ -397,10 +387,10 @@ export default {
         this.$confirm("要继续上传附件吗?", "提示", {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
-          showCancelButton:true
+          showCancelButton: true
         })
           .then(result => {
-            console.log(result)
+            console.log(result);
             if (result == "confirm") {
               let _this = this;
               setTimeout(function() {
@@ -408,20 +398,17 @@ export default {
               }, 500);
               //  _this.reload();
             }
-            
           })
           .catch(err => {
-            console.log(err)
+            console.log(err);
             let _this = this;
-            if(err){
+            if (err) {
               _this.reload();
-              setTimeout(function(){
+              setTimeout(function() {
                 _this.$router.push("/document");
-              },1000)
-             
+              }, 1000);
             }
           });
-        
       } else {
       }
       //
@@ -446,19 +433,18 @@ export default {
       //   this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL();
     },
     open3(msg) {
-      if(msg == undefined){
+      if (msg == undefined) {
         msg = "上传附件成功!";
-      }else{
-        msg = "上传文件成功!"
+      } else {
+        msg = "上传文件成功!";
       }
       this.$notify({
-          title: '成功',
-          message:msg,
-          type: 'success'
-        });
-      },
+        title: "成功",
+        message: msg,
+        type: "success"
+      });
+    },
     submit() {
-      
       this.chooseTags = [];
       let tags = this.form.tags;
       let _this = this;
@@ -475,11 +461,11 @@ export default {
         names.push(_this.chooseTags[i].tagName);
       }
 
-      for(let i = 0;i < _this.value.length;i++){
+      for (let i = 0; i < _this.value.length; i++) {
         values.push(_this.value[i]);
       }
       console.log("form:" + this.form.tags);
-      let date = moment(this.form.date).format("YYYY-MM-DD HH:mm:ss")
+      let date = moment(this.form.date).format("YYYY-MM-DD HH:mm:ss");
       this.table.department = this.form.department;
       this.table.region = this.form.region;
       this.table.type = this.value;
@@ -487,6 +473,7 @@ export default {
       this.table.number = this.form.number;
       this.table.tags = names;
       this.table.pageNum = this.form.pageNum;
+      this.table.fileSource = this.form.fileSource;
       console.log(this.table);
       console.log("this.table");
       this.$refs.upload.submit();
@@ -512,18 +499,16 @@ export default {
       this.$refs.uploadannex.submit();
     },
     uploadAnneixSuccess() {
-      
       this.open3();
       console.log(this.fileList);
       this.reload();
       this.dialogVisible = false;
 
-     let _this = this;
-     setTimeout(function(){
-        _this.$router.push("/document")
-     },1000)
-     
-      
+      let _this = this;
+      setTimeout(function() {
+        _this.$router.push("/document");
+      }, 1000);
+
       // this.reload();
     },
     showInput() {
@@ -544,24 +529,40 @@ export default {
       }
       this.inputVisible = false;
       this.inputValue = "";
+    },
+    getAllFileSource(){
+      let _this = this;
+      let url = "";
+      if (process.env.NODE_ENV === "development") {
+        url = "/api/getAllFileSource";
+      } else {
+        url = "/getAllFileSource";
+      }
+      postJsonRequest(url).then((result) => {
+        console.log(result);
+        this.fileSources = result.data.data;
+      }).catch((err) => {
+        
+      });
+
     }
   },
   created() {
     // this.reload();
     this.cropImg = this.defaultSrc;
-  },
-  
+    this.getAllFileSource();
+  }
 };
 </script>
 
 <style scoped>
-.el-upload-list el-upload-list--text{
+.el-upload-list el-upload-list--text {
   width: 100px !important;
 }
-.el-upload-list__item is-ready{
+.el-upload-list__item is-ready {
   width: 100px !important;
 }
-.el-upload-list__item-name{
+.el-upload-list__item-name {
   width: 100px !important;
 }
 .content-title {
@@ -607,9 +608,7 @@ export default {
 
 .el-tag + .el-tag {
   margin-left: 10px;
-  
 }
-
 
 .button-new-tag {
   margin-left: 10px;
